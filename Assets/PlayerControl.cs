67b01adf4442990e27b6607bfbb7140c2de3feec
irem <iremaydinn.7@gmail.com>
2023-04-28 10:30:38 +0300

    fusion import

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.TextCore.Text;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float speedSmoothVelocity = 0f;
+    [SerializeField] private float speedSmoothTime = 0.1f;
+
+    public float currentSpeed = 0f;
+    private CharacterController charController;
+    private Animator anim;
+    private float targetSpeed = 0f;
+
+    public void Start()
     {
-        
+        charController = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        Move();
+
+        if(Input.GetKey("T"))
+        {
+            anim.SetBool("Talk", true);
+        }
     }
+
+    public void Move()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        targetSpeed = walkSpeed * inputMagnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        charController.Move(movementDirection * currentSpeed * Time.deltaTime);
+        Debug.Log(inputMagnitude);
+        movementDirection.Normalize();
+        if (inputMagnitude > 0)
+        {
+            anim.SetBool("Walk", true);
+        }
+        
+       
+    }
+    
 }
