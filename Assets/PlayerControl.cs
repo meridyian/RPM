4034b9dfd4bc85e2c95ee49ccbc1844ec20b28b6
irem <iremaydinn.7@gmail.com>
2023-04-28 18:59:35 +0300

    inputauthority fix

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using Fusion;
 using UnityEngine;
 
 
 public class PlayerControl : NetworkBehaviour
 {
+    public static PlayerControl Local { get; set; }
     public Animator characterAnimator;
     private NetworkCharacterControllerPrototype _cc;
+    public CinemachineVirtualCamera localCamera;
 
     private void Awake()
     {
         _cc = GetComponent<NetworkCharacterControllerPrototype>();
     }
+
+    public override void Spawned()
+    {
+        if (Object.HasInputAuthority)
+        {
+            Local = this;
+            Debug.Log("Spawned local player");
+        }
+        else
+        {
+            //CinemachineVirtualCamera localCamera = GetComponent<CinemachineVirtualCamera>();
+            localCamera.enabled = false;
+            
+            Debug.Log("spawned remote player");
+            
+            
+        }
+        
+        
+    }
+
+
+
     public override void FixedUpdateNetwork()
     {
         if (GetInput(out NetworkInputData data))
