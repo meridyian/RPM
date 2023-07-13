23c859fbc020f5638557aeb4a601477e386eaf6c
meridyian <74350878+meridyian@users.noreply.github.com>
2023-07-13 18:05:57 +0300

    stopmovement while typing

--- Assets/EnableMovement.cs
+++ Assets/EnableMovement.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 
 public class EnableMovement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-   
-
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
         PlayerControl.Local.canMove = false;
+        PlayerControl.Local.characterAnimator.enabled = false;
     }
-
     public void OnPointerExit(PointerEventData eventData)
     {
         PlayerControl.Local.canMove = true;
+        PlayerControl.Local.characterAnimator.enabled = true;
     }
 }
